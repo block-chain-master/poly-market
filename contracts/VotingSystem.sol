e0e79ea4b6d86afa43684e4c0bb8df541af27c50
ypMarkJo <ypjo@megazone.com>
2024-11-29 23:57:59 +0900

    add:투표 정보 반환을 위한 getVoteInfos 추가

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -75,14 +75,13 @@ contract VotingSystem {
     }
 
     // 투표 결과 가져오기
-    function getVoteResults(uint voteId) public view returns (uint winningOption, uint totalBetAmount, address[] memory winners, uint[] memory amounts,string[] memory options) {
+    function getVoteResults(uint voteId) public view returns (uint winningOption, uint totalBetAmount, address[] memory winners, uint[] memory amounts) {
         require(votes[voteId].creator != address(0), "Vote does not exist");
 
         uint totalPool = votes[voteId].totalBetAmount; // 전체 배팅 금액
         uint creatorReward = totalPool / 1000; // 0.1% 수수료
         uint remainingPool = totalPool - creatorReward; // 승자들에게 분배할 금액
-        options=votes[voteId].options;
-
+       
         // 가장 많은 투표를 받은 옵션을 찾기
         uint winningVotes = 0;
         winningOption = 0;
@@ -106,7 +105,21 @@ contract VotingSystem {
             amounts[i] = (getTotalBetForUser(voteId, winners[i], winningOption) * remainingPool) / totalWinnersBet;
         }
 
-        return (winningOption, totalPool, winners, amounts,options);
+        return (winningOption, totalPool, winners, amounts);
+    }
+
+    function getVoteInfos(uint voteId) public view returns (string memory question,string memory imageURL,string[] memory options,uint[] memory votesPerOption){
+        question=votes[voteId].question;
+        imageURL=votes[voteId].imageURL;
+        options=votes[voteId].options;
+        votesPerOption = new uint[](votes[voteId].options.length); // 각 옵션별 투표된 갯수를 저장할 배열 초기화
+
+        // 각 옵션에 대해 투표된 갯수를 계산
+        for (uint i = 0; i < votes[voteId].options.length; i++) {
+            votesPerOption[i] = votes[voteId].bets[i].length; // 각 옵션에 대한 배팅 수 (투표된 갯수)
+        }
+
+        return (question,imageURL,options,votesPerOption);
     }
 
     // 승자 목록 반환
